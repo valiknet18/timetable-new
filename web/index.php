98efbf62dac02b0e362886b74f51e90eefdec83c
Valik Grinevich <valik.v1per@gmail.com>
2015-05-10 14:26:46 +0300

    update front controller

--- web/index.php
+++ web/index.php
@@ -1,7 +1,16 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Yaml\Yaml;
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->run();
+require_once __DIR__.'/../app/app.php';
+require_once __DIR__.'/../app/config/routing.php';
+
+$app['config'] = Yaml::parse(file_get_contents('../app/config/config.yml'));
+
+$request = Request::createFromGlobals();
+$app->run($request);
