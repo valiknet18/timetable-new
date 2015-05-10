f369665274f91f72a8ae8879e43520b872d8e28d
Valik Grinevich <valik.v1per@gmail.com>
2015-05-10 15:36:35 +0300

    change from abstract model to model

--- src/Valiknet/Model/Model.php
+++ src/Valiknet/Model/Model.php
@@ -2,9 +2,38 @@
 
 namespace Valiknet\Model;
 
-interface Model
-{
-    public function save();
+use Silex\Application;
 
-    public static function findOne(string $table_name, $pair);
+class Model implements InterfaceModel
+{
+    protected PDO $pdo;
+
+    public function __construct(Application $app)
+    {
+        $dsn = sprintf('pgsql:host=%s;dbname=%s', $app['config']['database']['host'], $app['config']['database']['database_name']);
+        $this->pdo = new \PDO($dsn, $app['config']['database']['username'], $app['config']['database']['password']);
+        $this->pdo->setAttribute( \PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION );
+        $this->pdo->exec("SET CHARACTER SET utf8");
+    }
+
+    public function getPdo()
+    {
+        return $this->pdo;
+    }
+
+    public static function findOne(string $sql, Pair $parameters)
+    {
+        $findOne = self::getPdo()->prepare($sql);
+        $result = $findOne->execute(array($parameters[0], $parameters[1]));
+
+        return $result;
+    }
+
+    public static function find(string $sql, Pair $pair)
+    {
+        $find = self::getPdo()->prepare($sql);
+        $result = $find->execute(array($pair[0], $pair[1]));
+
+        return $result;
+    }
 }
