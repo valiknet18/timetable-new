4de5b0c9953d7a017e28dbc9fadd129f988b9c48
Valik Grinevich <valik.v1per@gmail.com>
2015-05-10 15:37:05 +0300

    update abstract controller

--- src/Valiknet/Controller/AbstractController.php
+++ src/Valiknet/Controller/AbstractController.php
@@ -2,10 +2,14 @@
 
 namespace Valiknet\Controller;
 
+use Silex\Application;
+
 abstract class AbstractController
 {
-    public function __construct()
-    {
+    protected Application $app;
 
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
     }
 }
