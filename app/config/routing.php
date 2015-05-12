f971d61afc62b410433d306ed9543be2446d8ac1
Valik Grinevich <valik.v1per@gmail.com>
2015-05-12 13:19:53 +0300

    add routing

--- app/config/routing.php
+++ app/config/routing.php
@@ -1,3 +1,34 @@
 <?php
 
+//Events
 $app->get('/', 'Valiknet\Controller\EventsController::indexAction');
+
+//Groups
+$app->get('/groups', 'Valiknet\Controller\GroupsController::indexAction');
+
+//Subject
+$app->get('/subjects', 'Valiknet\Controller\SubjectsController::indexAction');
+
+//Auditories
+$app->get('/auditories', 'Valiknet\Controller\AuditoriesController::indexAction');
+
+//Teachers
+$app->get('/teachers', 'Valiknet\Controller\TeachersController::indexAction');
+
+
+//Admin
+
+//Events
+$app->get('/admin', 'Valiknet\Controller\Admin\EventsController::indexAction');
+
+//Groups
+$app->get('/admin/groups', 'Valiknet\Controller\Admin\GroupsController::indexAction');
+
+//Subject
+$app->get('/admin/subjects', 'Valiknet\Controller\Admin\SubjectsController::indexAction');
+
+//Auditories
+$app->get('/admin/auditories', 'Valiknet\Controller\Admin\AuditoriesController::indexAction');
+
+//Teachers
+$app->get('/admin/teachers', 'Valiknet\Controller\Admin\TeachersController::indexAction');
