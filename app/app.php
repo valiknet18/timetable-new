2e7d74271ad9929ad1d6bdb094656d3dc2aebef6
Valik Grinevich <valik.v1per@gmail.com>
2015-05-12 13:17:24 +0300

    update app and config file

--- app/app.php
+++ app/app.php
@@ -1,5 +1,14 @@
 <?php
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
+    'twig.options' => array(
+        'cache' => isset($app['twig.options.cache']) ? $app['twig.options.cache'] : false,
+        'strict_variables' => true,
+        'globals' => array('loginCheckRoute' => 'wXMgt4EB3pG9Jki35t6bpGLJMrkQces6ETTE9fkQ4JM'),
+    ),
+    'twig.path' => array(__DIR__ . '/views')
 ));
+
+$app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
+    return $twig;
+}));
