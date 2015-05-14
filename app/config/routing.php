1198b644c6494981222177f7fd010c0b994d5dd6
Valik Grinevich <valik.v1per@gmail.com>
2015-05-14 21:50:18 +0300

    create add and store Action

--- app/config/routing.php
+++ app/config/routing.php
@@ -11,6 +11,8 @@ $app->get('/subjects', 'Valiknet\Controller\SubjectsController::indexAction');
 
 //Auditories
 $app->get('/auditories', 'Valiknet\Controller\AuditoriesController::indexAction');
+$app->get('/auditories/{auditory_number}', 'Valiknet\Controller\AuditoriesController::viewAction')
+->bind('view_auditory_client');
 
 //Teachers
 $app->get('/teachers', 'Valiknet\Controller\TeachersController::indexAction');
@@ -28,7 +30,11 @@ $app->get('/admin/groups', 'Valiknet\Controller\Admin\GroupsController::indexAct
 $app->get('/admin/subjects', 'Valiknet\Controller\Admin\SubjectsController::indexAction');
 
 //Auditories
-$app->get('/admin/auditories', 'Valiknet\Controller\Admin\AuditoriesController::indexAction');
+$app->get('/admin/auditories', 'Valiknet\Controller\Admin\AuditoriesController::indexAction')
+    ->bind('list_auditories_admin');
+$app->get('/admin/auditories/new', 'Valiknet\Controller\Admin\AuditoriesController::createAction');
+$app->post('/admin/auditories/store', 'Valiknet\Controller\Admin\AuditoriesController::storeAction')
+    ->bind('store_auditory');
 
 //Teachers
 $app->get('/admin/teachers', 'Valiknet\Controller\Admin\TeachersController::indexAction');
--- src/Valiknet/Controller/Admin/AuditoriesController.php
+++ src/Valiknet/Controller/Admin/AuditoriesController.php
@@ -4,17 +4,31 @@ namespace Valiknet\Controller\Admin;
 
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Valiknet\Controller\AbstractController;
 use Valiknet\Model\Auditory;
 use Valiknet\Model\Model;
 
 class AuditoriesController extends AbstractController
 {
-    public function indexAction(Application $app, Request $request)
+    public function indexAction(Application $app)
     {
         $auditories = Auditory::findBy();
 
         return $app['twig']->render('admin/auditories/index.html.twig', ['auditories' => $auditories]);
+    }
+
+    public function createAction(Application $app)
+    {
+        return $app['twig']->render('/admin/auditories/create.html.twig');
+    }
+
+    public function storeAction(Application $app, Request $request)
+    {
+        $auditory = new Auditory();
+
+        $auditory->auditory_number = $request->request->get('auditory_number');
+        $auditory->auditory_type = $request->request->get('auditory_type');
+
+        return $app->redirect($app->path('list_auditories_admin'));
     }
 }
