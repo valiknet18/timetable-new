7e56bbb256cfdd9c7f590f19bf4e3c834aa0caed
Valik Grinevich <valik.v1per@gmail.com>
2015-06-03 00:40:10 +0300

    update design application

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -64,11 +64,16 @@ $(function(){
                     options += "<option value='" + subject.subject_code + "'>" + subject.subject_name + "</option>";
                 }
 
-                var select = "<div><label>Виберіть предмет</label><select name='subject' id='subject'>" + options +  "</select></div>";
+                var select = "<div class='form-group'>" +
+                                "<label for='subject' class='control-label col-sm-2'>Виберіть предмет</label>" +
+                                "<div class='col-sm-9'>" +
+                                    "<select name='subject' id='subject' class='form-control'>" + options +  "</select>" +
+                                "</div>" +
+                             "</div>";
 
-                $('#subject').parent().remove();
+                $('#subject').parent().parent().remove();
 
-                self.after(select);
+                self.parent().parent().after(select);
             }
         )
     });
@@ -91,14 +96,24 @@ $(function(){
                         options += "<option value='" + group.group_code + "'>" + group.group_name + "</option>";
                     }
 
-                    var select = "<div><label>Виберіть групи</label><select name='groups[]' id='groups' multiple>" + options +  "</select></div>";
+                    var select = "<div class='form-group'>" +
+                                    "<label for='groups' class='control-label col-sm-2'>Виберіть групу</label>" +
+                                    "<div class='col-sm-9'>" +
+                                        "<select name='groups[]' class='form-control' id='groups' multiple>" + options +  "</select>" +
+                                    "</div>" +
+                                 "</div>";
 
                 } else {
-                    var select = "<div><label>Виберіть групи</label><span id='groups'>В цьому курсі немає ні одної группи</span></div>";
+                    var select = "<div class='form-group'>" +
+                                    "<label for='groups' class='control-label col-sm-2'>Виберіть групи</label>" +
+                                    "<div class='col-sm-9'>" +
+                                        "<span id='groups'>До цього року не прив’язана ні одна група</span>" +
+                                    "</div>" +
+                                "</div>";
                 }
 
-                $('#groups').parent().remove();
-                self.after(select);
+                $('#groups').parent().parent().remove();
+                self.parent().parent().after(select);
             }
         )
     });
@@ -122,14 +137,24 @@ $(function(){
                         options += "<option value='" + auditory.auditory_number + "'>" + auditory.auditory_number + "</option>";
                     }
 
-                    select = "<div><label>Виберіть аудиторію </label><select name='auditory' id='auditory'>" + options +  "</select></div>";
+                    select = "<div class='form-group'>" +
+                                "<label for='auditory' class='control-label col-sm-2'>Виберіть аудиторію </label>" +
+                                "<div class='col-sm-9'>" +
+                                    "<select name='auditory' id='auditory' class='form-control'>" + options +  "</select>" +
+                                "</div>" +
+                             "</div>";
 
                 } else {
-                    select = "<div><label>Виберіть аудиторію </label><span id='auditory'>Цього типу немає ні однієї аудиторії</span></div>";
+                    select = "<div class='form-group'>" +
+                                "<label class='control-label col-sm-2'>Виберіть аудиторію </label>" +
+                                "<div class='col-sm-9'>" +
+                                    "<span id='auditory'>Цього типу немає ні однієї аудиторії</span>" +
+                                "</div>" +
+                            "</div>";
                 }
 
-                $('#auditory').parent().remove();
-                self.after(select);
+                $('#auditory').parent().parent().remove();
+                self.parent().parent().after(select);
             }
         )
     });
